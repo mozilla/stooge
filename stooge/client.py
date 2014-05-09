5e7918a77728d64c214d27c3be2d557a900777da
Stefan Arentz <stefan@arentz.ca>
2014-05-09 15:41:48 +0000

    Fixes #6 Move code to start a scan out of stooge-ctl

---
+++ stooge/client.py
@@ -0,0 +1,38 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+
+import datetime
+
+from celery.execute import send_task
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+from stooge.scanner.celery import celery
+from stooge.scanner.tasks import start_scan, finish_scan, execute_scan
+
+
+def scan():
+
+    client = MongoClient()
+    db = client.stooge
+
+    scan = {"created": datetime.datetime.utcnow(),
+            "state": "CREATED",
+            "started": None,
+            "finished": None,
+            "sites": []}
+
+    for site in db.sites.find():
+        scan["sites"].append({"_id": site["_id"],
+                              "responses": {"http":[], "https":[]},
+                              "results": {},
+                              "ssllabs": None,
+                              "error": None,
+                              "url": site["url"],
+                              "bugs": None})
+
+    scan_id = db.scans.insert(scan)
+
+    send_task("stooge.scanner.tasks.execute_scan", [str(scan_id)])
